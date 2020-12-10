a80a57e488a978f8ac247151dbe03895e91e83e0
Kim Palao <kimfarhantpalao@gmail.com>
2020-12-10 16:25:16 +0000

    Add migration for populating the database with values and principles. Add initial test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Value, Principle
 
 # Create your tests here.
+
+
+class ValueTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_value_insert(self):
+        response = self.client.put("/api/values", {"text": "This is a test"})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json["id"], 5)
