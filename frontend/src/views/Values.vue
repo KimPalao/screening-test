9ca00b9c214289cde6f7858a9a3d4fa2995246c1
Kim Palao <kimfarhantpalao@gmail.com>
2020-12-11 07:11:59 +0000

    Make new value form functional

--- frontend/src/views/Values.vue
+++ frontend/src/views/Values.vue
@@ -7,6 +7,33 @@
         </v-col>
       </v-row>
       <v-row>
+        <v-col>
+          <v-form @submit.prevent="insert">
+            <strong>Add New Value</strong>
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="10">
+                  <v-text-field
+                    v-model="new_object.text"
+                    label="Text"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="2"
+                  class="d-flex flex-column justify-center"
+                >
+                  <v-btn rounded color="primary" dark type="submit"
+                    >Submit</v-btn
+                  >
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-col>
+      </v-row>
+      <v-row>
         <v-col cols="auto">&nbsp;</v-col>
         <v-col><strong>Text</strong></v-col>
         <v-col cols="auto"></v-col>
@@ -27,6 +54,9 @@ export default {
   data() {
     return {
       objects: [],
+      new_object: {},
+      submitting: false,
+      loading: false,
     };
   },
   methods: {
@@ -41,6 +71,20 @@ export default {
           }
         );
         this.objects = response.data.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async insert() {
+      this.submitting = true;
+      try {
+        const response = await axios.put(
+          `${process.env.VUE_APP_BACKEND_URL}/api/values`,
+          this.new_object
+        );
+        if (response.status === 201) {
+          this.get_data();
+        }
       } catch (e) {
         console.log(e);
       }
