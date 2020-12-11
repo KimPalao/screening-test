16d0626be7f9b2f373c99d685e85fdb123b0f6a1
Kim Palao <kimfarhantpalao@gmail.com>
2020-12-11 06:28:57 +0000

    Add vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/values">Values</router-link> |
-      <router-link to="/principles">Principles</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        <router-link to="/" class="white--text"> Home </router-link>
+      </v-btn>
+      <v-btn text>
+        <router-link to="/values" class="white--text"> Values </router-link>
+      </v-btn>
+      <v-btn text>
+        <router-link to="/principles" class="white--text">
+          Principles
+        </router-link>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  components: {},
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
