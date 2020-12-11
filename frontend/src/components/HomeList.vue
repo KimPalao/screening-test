243679bcdb21ee4d980f6d15cd3e993e705db048
Kim Palao <kimfarhantpalao@gmail.com>
2020-12-11 06:03:29 +0000

    Add axios. Add a component for displaying the values and principles

---
+++ frontend/src/components/HomeList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "MainList",
+  data() {
+    return {
+      values: [],
+      principles: [],
+    };
+  },
+  methods: {
+    async get_values() {
+      try {
+        const response = await axios.get(
+          `${process.env.VUE_APP_BACKEND_URL}/api/values`,
+          {
+            params: {
+              all: true,
+            },
+          }
+        );
+        this.values = response.data.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async get_principles() {
+      try {
+        const response = await axios.get(
+          `${process.env.VUE_APP_BACKEND_URL}/api/principles`,
+          {
+            params: {
+              all: true,
+            },
+          }
+        );
+        this.principles = response.data.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  mounted() {
+    this.get_values();
+    this.get_principles();
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <home-list />
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import HomeList from "../components/HomeList.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { HomeList },
 };
 </script>
