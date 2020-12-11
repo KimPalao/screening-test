c86207c58ab278c2aad71ea3a6028bf26983c252
Kim Palao <kimfarhantpalao@gmail.com>
2020-12-11 07:46:02 +0000

    Create ProgressOverlay component. Place ProgressOverlay component in form in Values

---
+++ frontend/src/components/ProgressOverlay.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-overlay :absolute="true" :value="value">
+    <v-progress-circular
+      :size="size"
+      :color="color"
+      :indeterminate="indeterminate"
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+<script>
+export default {
+  name: "ProgressOverlay",
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+    indeterminate: {
+      type: Boolean,
+      default: true,
+    },
+    size: {
+      type: Number,
+      default: 50,
+    },
+    color: {
+      type: String,
+      default: "primary",
+    },
+  },
+};
+</script>
--- frontend/src/views/Values.vue
+++ frontend/src/views/Values.vue
@@ -9,27 +9,30 @@
       <v-row>
         <v-col>
           <v-form @submit.prevent="insert">
-            <strong>Add New Value</strong>
-            <v-container>
-              <v-row>
-                <v-col cols="12" md="10">
-                  <v-text-field
-                    v-model="new_object.text"
-                    label="Text"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                  cols="12"
-                  md="2"
-                  class="d-flex flex-column justify-center"
-                >
-                  <v-btn rounded color="primary" dark type="submit"
-                    >Submit</v-btn
+            <progress-overlay :value="submitting" />
+            <fieldset :disabled="submitting" class="pa-4">
+              <strong>Add New Value</strong>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="10">
+                    <v-text-field
+                      v-model="new_object.text"
+                      label="Text"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="2"
+                    class="d-flex flex-column justify-center"
                   >
-                </v-col>
-              </v-row>
-            </v-container>
+                    <v-btn rounded color="primary" dark type="submit"
+                      >Submit</v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </v-container>
+            </fieldset>
           </v-form>
         </v-col>
       </v-row>
@@ -46,11 +49,23 @@
     </v-container>
   </div>
 </template>
-
+<style scoped>
+form {
+  position: relative;
+}
+fieldset {
+  border: 0;
+}
+</style>
 <script>
 import axios from "axios";
+import ProgressOverlay from "@/components/ProgressOverlay.vue";
+
 export default {
-  name: "Home",
+  name: "Values",
+  components: {
+    ProgressOverlay,
+  },
   data() {
     return {
       objects: [],
@@ -87,6 +102,8 @@ export default {
         }
       } catch (e) {
         console.log(e);
+      } finally {
+        this.submitting = false;
       }
     },
   },
